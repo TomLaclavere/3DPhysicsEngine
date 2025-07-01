fd859cac1798275aa06bd14141a282daabbae5af
TomLaclavere <laclavere@apc.in2p3.fr>
2025-07-01 21:55:56 +0200

    Add shortcut _d for decimal

--- lib/precision.hpp
+++ lib/precision.hpp
@@ -5,3 +5,7 @@ using decimal = double;
 #else
 using decimal = float;
 #endif
+
+// define literal for decimal type
+constexpr decimal operator"" _d(long double val) { return static_cast<decimal>(val); }
+constexpr decimal operator"" _d(unsigned long long val) { return static_cast<decimal>(val); }

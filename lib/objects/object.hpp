70b22061c9a378bb1dffd79d0d224157ae0e27fa
TomLaclavere <laclavere@apc.in2p3.fr>
2025-09-02 14:43:42 +0200

    Start writting functions of Object class

--- lib/objects/object.hpp
+++ lib/objects/object.hpp
@@ -8,8 +8,38 @@ private:
     Vector3D position;
     Vector3D rotation;
     Vector3D scale;
+    decimal  mass;
 
 public:
-    Object()  = default;
-    ~Object() = default;
+    // ============================================================================
+    // ============================================================================
+    //  Constructors
+    // ============================================================================
+    // ============================================================================
+    Object() = default;
+    Object(const Vector3D& position = Vector3D(), const Vector3D& rotation = Vector3D(),
+           const Vector3D& scale = Vector3D(), decimal mass = 0)
+        : position { position }
+        , rotation { rotation }
+        , scale { scale }
+        , mass { mass }
+    {}
+
+    // ============================================================================
+    // ============================================================================
+    //  Getters
+    // ============================================================================
+    // ============================================================================
+    Vector3D get_position() const { return position; }
+    Vector3D get_rotation() const { return rotation; }
+    Vector3D get_scale() const { return scale; }
+
+    // ============================================================================
+    // ============================================================================
+    //  Transformations
+    // ============================================================================
+    // ============================================================================
+    void translation(const Vector3D& v_translation) { position += v_translation; }
+    void rotation(const Vector3D& v_rotation) { rotation += v_rotation; }
+    void scaling(const Vector3D& v_scaling) { scale += v_scaling; }
 };

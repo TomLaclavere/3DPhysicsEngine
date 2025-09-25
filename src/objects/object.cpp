01d2a88a2899aacbcaa5f20bd610711c261ffc45
TomLaclavere <tomlaclavere@gmail.com>
2025-09-25 19:30:47 +0000

    Document objects files

--- src/objects/object.cpp
+++ src/objects/object.cpp
@@ -1,9 +1,18 @@
+/**
+ * @file object.cpp
+ * @brief Implementation of Object class methods.
+ *
+ * Implements all member functions of the Object class declared in object.hpp,.
+ *
+ * This file includes:
+ *  - Integration of motion equations.
+ *
+ * @see object.hpp
+ */
 #include "objects/object.hpp"
 
 // ============================================================================
-// ============================================================================
 //  Apply forces / torques
-// ============================================================================
 // ============================================================================
 /**
  * Default implementation uses simple Euler integration.
